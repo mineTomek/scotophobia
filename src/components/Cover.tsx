45f8a037fc579e63b676ee2969589661d4e886bd
mineTomek <lukom.tomek@gmail.com>
2025-05-01 21:15:37 +0200

    Update the cover

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -1,12 +1,9 @@
 function Cover() {
     return (
-      <div className="w-[512px] relative h-[256px] bg-gradient-to-b from-emerald-50 to-emerald-100 flex flex-col items-center justify-center gap-5 m-5">
+      <div className="w-[512px] group relative h-[256px] bg-gradient-to-b from-emerald-50 to-emerald-100 flex flex-col items-center justify-center gap-5 m-5">
         <h1 className="text-4xl font-bold mb-4 title">Scotophobia</h1>
-        <span className="opacity-70 max-w-1/2 text-center text-sm">
-          "Helping" people with scotophobia since... not that long ago
-        </span>
 
-        <div className="absolute h-64 w-64 border-2 border-black hidden" />
+        <div className="absolute h-64 w-64 border-2 border-black/30 rounded-2xl hidden group-hover:block" />
       </div>
     );
 }
