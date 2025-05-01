c25df28a515b570ae5f977eb7c45117a352260fa
mineTomek <lukom.tomek@gmail.com>
2025-05-01 20:28:26 +0200

    Move the cover into its own component

---
+++ src/components/Cover.tsx
@@ -0,0 +1,12 @@
+function Cover() {
+    return (
+      <div className="w-[512px] h-[256px] bg-gradient-to-b from-emerald-50 to-emerald-100 flex flex-col items-center justify-center gap-5 m-5">
+        <h1 className="text-7xl font-bold mb-4 title">Scotophobia</h1>
+        <span className="opacity-70 max-w-2/3 text-center text-lg.">
+          "Helping" people with scotophobia since... not that long ago
+        </span>
+      </div>
+    );
+}
+
+export default Cover;
